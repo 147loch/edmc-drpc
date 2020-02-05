8b42cbcda66090319450b64df16d240695d18795
Loïc Herman <mail@147lo.ch>
2020-02-05 23:26:34 +0100

    chore: up the version

--- load.py
+++ load.py
@@ -2,7 +2,7 @@
 EDMC Discord Rich Presence Plugin
 :author: 147loch
 :date: 05.02.2020
-:version: 2.1.2
+:version: 2.1.3
 """
 
 from os.path import dirname, join
@@ -24,7 +24,7 @@ _ = functools.partial(l10n.Translations.translate, context=__file__)
 
 CLIENT_ID = b'522871175420837901'
 
-VERSION = '2.1.2'
+VERSION = '2.1.3'
 
 # Add global var for Planet name (landing + around)
 planet = '<Hidden>'
